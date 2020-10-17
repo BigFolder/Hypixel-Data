b121b0d2505d01e7a229dc901c2a8d6365401ed4
BigFolder <48892041+BigFolder@users.noreply.github.com>
2020-10-17 14:11:45 -0400

    Update Hypixel_Invest_Example.py
    Updated to invcude new spreadsheet pages

--- Bazaar_Spreadsheet_Example/Hypixel_Invest_Example.py
+++ Bazaar_Spreadsheet_Example/Hypixel_Invest_Example.py
@@ -1,6 +1,7 @@
 import gspread
 import time
 from datetime import datetime
+import bazaarCraftFlips
 import HypixelBazaar
 import sys
 
@@ -11,7 +12,35 @@ takes the newestData (currentData), crashData(getCrashData) and the location of 
 '''
 
 
-def updateShuffleSheet(newData, gsheetAPI, sheetName):
+def updateCraftFlippingSheet(newData, gsheetAPI, sheetName):
+    # Create connection to Google Spreadsheet api
+    gc = gspread.service_account(gsheetAPI)
+    # Name of the sheet we want to interact with.
+    wks = gc.open(sheetName).get_worksheet(4)
+    wks.clear()
+    # Updating the "Last Updated" date time in the sheet.
+    wks.update('A1', [['Spreadsheet Last Updated ', str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")) + " EST",
+                       '"NA" Means not on AH  ||  Sugar Cane -> Paper  ||  Wood -> sticks']])
+
+    n = 4
+
+    gc.login()
+
+    for prod in newData:
+        wks.update('A'+str(n)+':G'+str(n), [["Enchant Name", "Bazaar Cost", "Auction Value(BIN)", "Current Profit",
+                                             "Product Needed", "Total Cost", "Amount Needed"]])
+        n += 1
+        wks.update('A'+str(n)+':D'+str(n), [[prod[0], prod[1]["bzCost"], prod[1]["ahCost"], prod[1]["minProfit"]]])
+
+        for item in prod[1]["items"]:
+            wks.update('E'+str(n)+':G'+str(n), [[item[0], item[1], item[2]]])
+            n += 1
+
+        n += 3
+    print("Done with Bazaar Enchant Flips")
+
+
+def updateShuffleSheet(newData, gsheetAPI, sheetName, minionShuffles):
     # Create connection to Google Spreadsheet api
     gc = gspread.service_account(gsheetAPI)
     # Name of the sheet we want to interact with.
@@ -32,6 +61,15 @@ def updateShuffleSheet(newData, gsheetAPI, sheetName):
                                              round(sellFor, 2),
                                              round(profit, 2)]])
         n += 1
+
+    n = 4
+
+    for item in minionShuffles:
+        wks.update("F"+str(n)+":I"+str(n), [[item, minionShuffles[item]["vendorSell"],
+                                             minionShuffles[item]["instaSell"],
+                                             minionShuffles[item]["slowSell"]]])
+        n+= 1
+
     print("Done with Merchant Shuffles")
 
 
@@ -40,7 +78,10 @@ def updateInsightGoogleSheet(insight, gsheetAPI, sheetName):
     # Name of the sheet we want to interact with.
     wks = gc.open(sheetName).get_worksheet(2)
     # Updating the "Last Updated" date time in the sheet.
-
+    scuffNames = {"LOG:1": "Spruce Wood", "LOG:3": "Jungle Wood", "LOG:2": "Birch Wood", "CARROT_ITEM": "Carrot",
+                  "LOG": "Oak Wood", "POTATO_ITEM": "Potato", "INK_SACK:3": "Cocoa Bean", "LOG_2": "Acacia Wood",
+                  "LOG_2:1": "Dark Oak Wood", "ENDER_STONE": "End Stone", "NETHER_STALK": "Nether Wart",
+                  "RAW_FISH:3": "Pufferfish", "RAW_FISH:1": "Raw Salmon", "RAW_FISH:2": "Clownfish"}
     gc.login()
     n = 2
     wks.update('A1', [['Spreadsheet Last Updated ', str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")) + " EST"]])
@@ -57,8 +98,12 @@ def updateInsightGoogleSheet(insight, gsheetAPI, sheetName):
                                                    (round(entireSoldFor - entireCost, 2)),
                                                    playerAmount]])
         for product in products:
+            if product in scuffNames:
+                prod = scuffNames[product]
+            else:
+                prod = product
 
-            wks.update('A' + str(n) + ":H" + str(n), [[product, (products[product]['itemQuantity']),
+            wks.update('A' + str(n) + ":H" + str(n), [[prod, (products[product]['itemQuantity']),
                                                        (round(products[product]['totalCost'])),
                                                        (round(products[product]['totalSold'])),
                                                        (round(products[product]['totalProfit'])),
@@ -78,15 +123,22 @@ def updateCrashesGoogleSheet(crashData, gsheetAPI, sheetName):
     # Name of the sheet we want to interact with.
     wks = gc.open(sheetName).get_worksheet(1)
     # Updating the "Last Updated" date time in the sheet.
-
+    scuffNames = {"LOG:1": "Spruce Wood", "LOG:3": "Jungle Wood", "LOG:2": "Birch Wood", "CARROT_ITEM": "Carrot",
+                  "LOG": "Oak Wood", "POTATO_ITEM": "Potato", "INK_SACK:3": "Cocoa Bean", "LOG_2": "Acacia Wood",
+                  "LOG_2:1": "Dark Oak Wood", "ENDER_STONE": "End Stone", "NETHER_STALK": "Nether Wart",
+                  "RAW_FISH:3": "Pufferfish", "RAW_FISH:1": "Raw Salmon", "RAW_FISH:2": "Clownfish"}
     gc.login()
     wks.update('A1', [['Spreadsheet Last Updated ', str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")) + " EST"]])
 
     n = 3
     for crashIndex in crashData:
         for crash in crashIndex:
+            if crash[0] in scuffNames:
+                crashName = scuffNames[crash[0]]
+            else:
+                crashName = crash[0]
 
-            wks.update('A' + str(n) + ":F" + str(n), [[crash[0], round(crash[4], 2), round(crash[3], 2),
+            wks.update('A' + str(n) + ":F" + str(n), [[crashName, round(crash[4], 2), round(crash[3], 2),
                                                        str(round(crash[5] * 100, 3)) + "%",
                                                        str(round(crash[1] * 100, 3)) + "%",
                                                        str(round(crash[2] * 100, 3)) + "%"]])
@@ -111,12 +163,23 @@ def updateGoogleSheet(newData, gsheetAPI, sheetName):
     n = 2
     count = 0
     gc.login()
+
+    scuffNames = {"LOG:1": "Spruce Wood", "LOG:3": "Jungle Wood", "LOG:2": "Birch Wood", "CARROT_ITEM": "Carrot",
+                  "LOG": "Oak Wood", "POTATO_ITEM": "Potato", "INK_SACK:3": "Cocoa Bean", "LOG_2": "Acacia Wood",
+                  "LOG_2:1": "Dark Oak Wood", "ENDER_STONE": "End Stone", "NETHER_STALK": "Nether Wart",
+                  "RAW_FISH:3": "Pufferfish", "RAW_FISH:1": "Raw Salmon", "RAW_FISH:2": "Clownfish"}
+
     for product in newData:
         if product[1]['reliability'] >= 7 and count <= 40 and product[1]['RoI'] >= 2:
+            if product[0] in scuffNames:
+                prodName = scuffNames[product[0]]
+            else:
+                prodName = product[0]
+
             count += 1
             n += 1
 
-            wks.update('A'+str(n)+":J"+str(n), [[product[0], (round(product[1]['RoI'], 2)/100),
+            wks.update('A'+str(n)+":J"+str(n), [[prodName, (round(product[1]['RoI'], 2)/100),
                                                  round(product[1]['margin'], 2),
                                                  float(product[1]['reliability']), round(product[1]['buyPrice'], 2),
                                                  round(product[1]['sellPrice'], 2), product[1]['buyOrders'],
@@ -147,13 +210,13 @@ def main(count):
         gsheetAPI1 = line["gsheetAPI1"]
         gsheetAPI2 = line["gsheetAPI2"]
         gsheetAPI3 = line["gsheetAPI3"]
+        gsheetAPI4 = line["gsheetAPI4"]
         sheet = "HypixelInvest"
         file.close()
 
     while True:
 
         try:
-
             # Clean the data and create predictions on the data
             currentData = HypixelBazaar.gather_bazaar_data(API_KEY=API_KEY, modelFile="finalized_multiclass_model.sav")
 
@@ -176,26 +239,46 @@ def main(count):
             testShuffle = HypixelBazaar.sellerShuffle(currentData)
 
             profitSort = HypixelBazaar.sortData(data=testShuffle, dataFeature="profit")
-            updateShuffleSheet(newData=profitSort, gsheetAPI=gsheetAPI3, sheetName=sheet)
 
+            minionShuffles = HypixelBazaar.merchantMinionShuffle(currentData)
+
+            updateShuffleSheet(newData=profitSort, gsheetAPI=gsheetAPI3, sheetName=sheet, minionShuffles=minionShuffles)
+
+
+            #finalSheet Bazaar Enchants
+
+            bazaarCosts = bazaarCraftFlips.bazaarPart()
+
+            auctionPrices = bazaarCraftFlips.auctionPart()
+
+            finalData = bazaarCraftFlips.compareData(bazaarCosts, auctionPrices)
+
+            sortedFlips = HypixelBazaar.sortData(data=finalData, dataFeature="minProfit")
+            time.sleep(20)
+            updateCraftFlippingSheet(newData=sortedFlips, gsheetAPI=gsheetAPI4, sheetName=sheet)
             # Saves the currentData in our CrashData file, ensuring it becomes the next "previous" data
             crashes = []
             crashes.append(HypixelBazaar.checkCrashData(currentData))
             HypixelBazaar.saveCrashData(data=currentData)
             count += 1
-            time.sleep(80)
+            print("Sheet last updated on", datetime.now())
+
 
             if count % 3 == 0:
                 updateCrashesGoogleSheet(crashData=crashes, gsheetAPI=gsheetAPI1, sheetName=sheet)
                 crashes = []
-                time.sleep(45)
+                time.sleep(50)
 
-            # Saves data every 10 iterations
-            if count % 10 == 0:
+
+            if count % 5 == 0:
                 HypixelBazaar.saveBazaarData(currentData)
-                time.sleep(40)
 
-            print("Sheet last updated on", datetime.now())
+                time.sleep(50)
+            else:
+                time.sleep(80)
+
+
+
 
         # Catches the only error we end up hitting, an api error with gspread that randomly occurs (I don't know)
         except:
